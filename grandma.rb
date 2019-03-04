26e8d514d161315ae8d019191e5d1148846310f7
Riccardo <riccardo_london@live.it>
2019-03-04 21:53:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(ask)
   ask1 = ask.upcase
-  if ask = "I LOVE YOU GRANDMA!"
+  if ask === "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif ask == ask1
+  elsif ask === ask1
       return "NO, NOT SINCE 1938!"
     else
         return "HUH?! SPEAK UP, SONNY!"
